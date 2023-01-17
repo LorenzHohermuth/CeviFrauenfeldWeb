23247b596353022912236658afe0c5e0f8717d56
Lorenz Hohermuth <lofloho@gmail.com>
2023-01-17 11:37:58 +0100

    svg canges

--- src/App.jsx
+++ src/App.jsx
@@ -12,8 +12,8 @@ function App() {
             <div className='absolute top-11'>Frauenfeld</div>
           </div>
         </div>
-        <div className='df bg-gradient-to-r from-cevi-red to-cevi-whiteRed'>
-          <img src="/Icons/Home.svg" alt="Home icon" />
+        <div className='bg-gradient-to-r from-cevi-red to-cevi-whiteRed h-9 mx-5'>
+          <img src="/Icons/white/Home.svg" className='' alt="Home icon" />
           dsjf
         </div>
       </nav>
